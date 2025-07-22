df3bbf402750b9176dd680827d0e2b1033cca94c
Urobobros <martin.burianek1987@gmail.com>
2025-07-22 10:57:35 +0200

    Add WHPX CPU option to IBM XT model

--- src/models/module/ibmxt.c
+++ src/models/module/ibmxt.c
@@ -4,5 +4,7 @@ void ibmxt_init_model() {
         xt_init();
 }
 
-MODEL m_ibmxt = {"[8088] IBM XT", ROM_IBMXT, "ibmxt", {{"", cpus_8088}, {"", NULL}, {"", NULL}}, MODEL_GFX_NONE, 64, 640, 64,
+MODEL m_ibmxt = {"[8088] IBM XT", ROM_IBMXT, "ibmxt",
+                 {{"", cpus_8088}, {"whpx", cpus_WHPX}, {"", NULL}},
+                 MODEL_GFX_NONE, 64, 640, 64,
                  ibmxt_init_model,         NULL};
